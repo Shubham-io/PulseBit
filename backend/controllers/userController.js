cd3450871f223ee79934a3315c5dfe75dab50a25
Shubham Vishwakarma <no_reply@gmail.com>
2025-06-07 22:23:14 +0530

    fix unused function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -184,36 +184,36 @@ const listAppointment = async (req, res) => {
 };
 
 // API to cancel appointments
-// const cancelAppointment = async (req, res) => {
-//   try {
-//     const { userId, appointmentId } = req.body;
-//     const appointmentData = await appointmentModel.findById(appointmentId);
+const cancelAppointment = async (req, res) => {
+  try {
+    const { userId, appointmentId } = req.body;
+    const appointmentData = await appointmentModel.findById(appointmentId);
 
-//     // verify appointment user
-//     if (appointmentData.userId !== userId) {
-//       return res.json({ success: false, message: "Unauthorized action" });
-//     }
+    // verify appointment user
+    if (appointmentData.userId !== userId) {
+      return res.json({ success: false, message: "Unauthorized action" });
+    }
 
-//     await appointmentModel.findByIdAndUpdate(appointmentId, {
-//       cancelled: true,
-//     });
-//     // releasing doctor slot
+    await appointmentModel.findByIdAndUpdate(appointmentId, {
+      cancelled: true,
+    });
+    // releasing doctor slot
 
-//     const { docId, slotDate, slotTime } = appointmentData;
-//     const doctorData = await doctorModel.findById(docId);
+    const { docId, slotDate, slotTime } = appointmentData;
+    const doctorData = await doctorModel.findById(docId);
 
-//     let slots_booked = doctorData.slots_booked;
-//     slots_booked[slotDate] = slots_booked[slotDate].filter(
-//       (e) => e !== slotTime
-//     );
+    let slots_booked = doctorData.slots_booked;
+    slots_booked[slotDate] = slots_booked[slotDate].filter(
+      (e) => e !== slotTime
+    );
 
-//     await doctorModel.findByIdAndUpdate(docId, { slots_booked });
-//     res.json({ success: true, message: "Appointment Cancelled" });
-//   } catch (error) {
-//     console.log(error);
-//     res.json({ success: false, message: error.message });
-//   }
-// };
+    await doctorModel.findByIdAndUpdate(docId, { slots_booked });
+    res.json({ success: true, message: "Appointment Cancelled" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 
 // API to make payment of appointment using razorpay
